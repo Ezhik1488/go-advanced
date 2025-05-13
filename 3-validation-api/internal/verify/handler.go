3bba9918198b8b8162654a2f61accbd12ebdcbaf
Alexandr <alexandr@biznes-len.com>
2025-05-13 16:24:55 +0300

    add: Полный цикл отправки и проверки Email

--- 3-validation-api/internal/verify/handler.go
+++ 3-validation-api/internal/verify/handler.go
@@ -3,8 +3,11 @@ package verify
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/go-playground/validator/v10"
 	"net/http"
+	"strings"
 	"validation-api/pkg/email_client"
+	"validation-api/pkg/hash"
 	"validation-api/pkg/storage"
 )
 
@@ -22,24 +25,41 @@ func NewVerifyHandler(router *http.ServeMux, email *email_client.EmailClient, st
 
 func (h *VerifyHandler) SendVerifyEmail() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		recipient := h.ec.Cfg.Email.Recipient
-		url := "http://localhost:8081/verify/" + recipient
-		// Сохранить в JSON hash email: hash
-		err := h.ls.Write(map[string]string{recipient: url})
+
+		var body VerifyRequest
+		err := json.NewDecoder(r.Body).Decode(&body)
 		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		validate := validator.New()
+		err = validate.Struct(&body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		hashMail := hash.GetMD5Hash(body.Email)
+
+		url := "http://localhost:8081/verify/" + hashMail
+		// Сохранить в JSON hash email: hash
+		err = h.ls.Write(map[string]string{hashMail: body.Email})
+		if err != nil {
+			fmt.Println(err)
 			return
 		}
 		// Отправить Email
-		err = h.ec.SendEmailWithTLS(recipient, url)
+		err = h.ec.SendEmailWithTLS(body.Email, url)
 		if err != nil {
+			fmt.Println(err)
 			return
 		}
 		// Отдать ответ клиенту
 		w.WriteHeader(http.StatusOK)
-		_, err = w.Write([]byte("Verification email sent"))
-		if err != nil {
-			return
-		}
+		w.Write([]byte("Verification email sent"))
 	}
 }
 
@@ -47,11 +67,28 @@ func (h *VerifyHandler) VerifyEmail() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
+
 		data, err := h.ls.Read()
 		if err != nil {
 			fmt.Println(err)
+			return
 		}
-		err = json.NewEncoder(w).Encode(data)
+
+		var mapData map[string]string
+		err = json.Unmarshal(data, &mapData)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		isValid := false
+
+		if _, ok := mapData[(strings.Split(r.URL.Path, "/"))[2]]; ok {
+			isValid = true
+		}
+
+		res := VerifyResponse{IsValid: isValid}
+		err = json.NewEncoder(w).Encode(res)
 		if err != nil {
 			return
 		}
