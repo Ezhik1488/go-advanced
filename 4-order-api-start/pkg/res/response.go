81c5cc1359e057f73d976a314b6f6367d155ac96
Aleksandr <aleksandr@telexchange.ee>
2025-05-16 01:14:18 +0300

    fix: delete pointer on http.ResponseWriter

--- 4-order-api-start/pkg/res/response.go
+++ 4-order-api-start/pkg/res/response.go
@@ -5,12 +5,12 @@ import (
 	"net/http"
 )
 
-func JSON(w http.ResponseWriter, data any, status int) *http.ResponseWriter {
+func JSON(w http.ResponseWriter, data any, status int) http.ResponseWriter {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(status)
 	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
 		return nil
 	}
-	return &w
+	return w
 }
