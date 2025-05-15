de73e34780a89223e789962e028db2e133951e0c
Aleksandr <aleksandr@telexchange.ee>
2025-05-16 01:18:26 +0300

    fix: add return in OPTION method in CORS

--- 4-order-api-start/pkg/middleware/cors.go
+++ 4-order-api-start/pkg/middleware/cors.go
@@ -12,6 +12,7 @@ func CORS(next http.Handler) http.Handler {
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 			w.Header().Set("Access-Control-Max-Age", "86400")
+			return
 		}
 		next.ServeHTTP(w, r)
 	})
